0e6279c06bf69259ffa3cc08a70e583271f74e8e
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-05 20:13:20 +0530

    add cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(function (req, res, next) {
   req.name = "Tyrex";
